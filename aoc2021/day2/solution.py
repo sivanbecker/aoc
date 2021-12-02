2feca4352741aa819da6e465f7f30cafde64bb5f
Sivan Becker <sivanbecker@gmail.com>
2021-12-02 09:16:12 +0200

    2021.day2 done

--- aoc2021/day2/solution.py
+++ aoc2021/day2/solution.py
@@ -1,9 +1,41 @@
 class DailyClass:
     def __init__(self, fh_iter):
         self._fh_iter = fh_iter
+        self.f = 0
+        self.d = 0
+        self.a = 0
+        
+    def forward_v2(self, f):
+        self.f += int(f)
+        self.d += self.a * int(f)
+
+    def forward(self, f):
+        self.f += int(f)
+
+    def down(self, d):
+        self.d += int(d)
+
+    def down_v2(self, d):
+        self.a += int(d)
+
+    def up(self, u):
+        self.d -= int(u)  
+
+    def up_v2(self, u):
+        self.a -= int(u)
+
+    def calc_position(self, part2=False):
+        move = {'up': self.up_v2 if part2 else self.up, 
+                "down": self.down_v2 if part2 else self.down, 
+                "forward": self.forward_v2 if part2 else self.forward}
+
+        for l in self._fh_iter:
+            cmd, amount = l.strip().split()
+            move[cmd](amount)
+        return self.f * self.d
 
     def solve_part1(self):
-        return "Not Impl"
+        return self.calc_position()
 
     def solve_part2(self):
-        return "Not Impl"
+        return self.calc_position(part2=True)
