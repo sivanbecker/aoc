5990bc06a942d31f1d60bf1ddd8c9681f48fd5fb
Sivan Becker <sivanbecker@gmail.com>
2021-12-03 22:16:48 +0200

    2021.day4 init

---
+++ aoc2021/day4/solution.py
@@ -0,0 +1,9 @@
+class DailyClass:
+    def __init__(self, fh_iter):
+        self._fh_iter = fh_iter
+
+    def solve_part1(self):
+        return "Not Impl"
+
+    def solve_part2(self):
+        return "Not Impl"
