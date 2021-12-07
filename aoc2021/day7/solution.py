f9d2efbedb1736027988c80bc22cb9a0a18fb74d
Sivan Becker <sivanbecker@gmail.com>
2021-12-07 09:18:43 +0200

    2021.day7 part1 naive solution

--- aoc2021/day7/solution.py
+++ aoc2021/day7/solution.py
@@ -1,9 +1,23 @@
+from collections import defaultdict
 class DailyClass:
     def __init__(self, fh_iter):
         self._fh_iter = fh_iter
+        self.positions = defaultdict(int)
+        self.costs = {}
+
+    def digest_input(self):
+         for p in next(self._fh_iter).strip().split(","):
+             self.positions[int(p)] += 1
+
+    def calc_all_costs(self):
+        for xi in self.positions:
+            self.costs[xi] = sum([abs(xi-xj)*self.positions[xj] for xj in self.positions if xj != xi])
 
     def solve_part1(self):
-        return "Not Impl"
+        self.digest_input()
+        self.calc_all_costs()
+        print(self.costs)
+        return min(self.costs.values())
 
     def solve_part2(self):
         return "Not Impl"
