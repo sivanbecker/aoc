5859e0c927e16f679e5ca85881653479346d33c7
Sivan Becker <sivanbecker@gmail.com>
2021-12-08 09:40:55 +0200

    add digest_input method to template

--- aoc2021/day8/solution.py
+++ aoc2021/day8/solution.py
@@ -2,15 +2,8 @@ class DailyClass:
     def __init__(self, fh_iter):
         self._fh_iter = fh_iter
 
-    def digest_input(self):
-        for l in self._fh_iter:
-            # do something with l
-            pass
-
     def solve_part1(self):
-        self.digest_input()
         return "Not Impl"
 
     def solve_part2(self):
-        self.digest_input()
         return "Not Impl"
