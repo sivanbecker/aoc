8b5d75f504b4ae33ce2ebd3a0aea8bbd5bb41048
Sivan Becker <sivanbecker@gmail.com>
2021-12-10 01:27:58 +0200

    2021.day8 struggling

--- aoc2021/day8/solution.py
+++ aoc2021/day8/solution.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 from functools import reduce
+from typing import final
 class DailyClass:
     
     # 0 - 6 segments - 
@@ -25,20 +26,130 @@ class DailyClass:
     def __init__(self, fh_iter):
         self._fh_iter = fh_iter
         self.uniq_output_values = [] 
-        self.seg_count_dig_options = {2: [1], 3: [7] ,4: [4], 7:[8], 5: [2,5], 6: [0,6,9]}
+        self.seg_count_dig_options = {2: [1], 3: [7] ,4: [4], 7:[8], 5: [2,3,5], 6: [0,6,9]}
         self.digit_to_actual_segments = defaultdict(list)
         self.inputs_2 = []
         self.outputs_2 = []
 
-    def digest_input(self):
+    def filter_uniq(self, st):
+        return len(st) in (2,3,4,7)
+
+    def collect_uniq(self, out_segments):
+        return list(filter(lambda st: self.filter_uniq(st), out_segments))
+
+    def analyse_segments(self, st):
+        if len(st) == 3:
+            self.digit_to_actual_segments[7] = st
+        elif len(st) == 2:
+            self.digit_to_actual_segments[1] = st
+            self.RU = list(st)
+            self.RD = self.RU
+
+        elif len(st) == 7:
+            self.digit_to_actual_segments[8] = st
+        elif len(st) == 4:
+            self.digit_to_actual_segments[4] = st
+
+    def disect(self):
+        if all(k in self.digit_to_actual_segments for k in (1,7)):
+            self.U = self.digit_to_actual_segments[7]
+            for _c in self.digit_to_actual_segments[1]:
+                self.U = self.U.replace(_c, '') 
+        if all(k in self.digit_to_actual_segments for k in (1,4)):
+            self.LU = self.digit_to_actual_segments[4]
+            for _c in self.digit_to_actual_segments[1]:
+                self.LU = self.LU.replace(_c, '') 
+            self.LU = list(self.LU)
+            self.M = self.LU
+
+    def map_digits(self, inp_segments, out_segments):
+        digits = {0: "", 1: "", 2: "", 3: "", 4: "",
+                    5: "",6: "", 7: "", 8: "", 9: ""}
+        line_out_uniqs = self.collect_uniq(out_segments)
+        line_in_uniqs = self.collect_uniq(inp_segments)
+        line_uniqs = set(line_out_uniqs + line_in_uniqs)
+        for pat in line_uniqs:
+            if len(pat) == 2:
+                digits[1] = pat
+            if len(pat) == 4:
+                digits[4] = pat
+            if len(pat) == 7:
+                digits[8] = pat
+            if len(pat) == 3:
+                digits[7] = pat
+        # find 9
+        almost9 = set(digits[4]).union(set(digits[7]))
+        for pat in inp_segments:
+            if len(pat) == 6:
+                if all(c in pat for c in almost9):
+                    digits[9] = pat
+        # find 6/0 (6 is like 8 without seg that exists in 1)
+        for pat in inp_segments:
+            if len(pat) == 6:
+                for c in digits[1]:
+                    if set(pat) == set(digits[8]).difference(c):     
+                        digits[6] = pat
+                    elif pat == digits[9]:
+                        continue
+                    else:
+                        digits[0] = pat
+        # find 5/2/3 - like 9 but without one segment
+        for pat in inp_segments:
+            if len(pat) == 5:
+                for c in digits[1]:
+                    if set(pat) == set(digits[9]).difference(c) and 5 not in digits:
+                        digits[5] = pat
+            if 5 in digits:
+                break
+
+        for pat in inp_segments:
+            if len(pat) == 5:
+                
+                diff_4_1 = set(digits[4]).difference(digits[1])
+                assert len(diff_4_1) == 2
+                for c in diff_4_1:
+                    if set(pat) == set(digits[9]).difference(c) and 3 not in digits:
+                        digits[3] = pat
+            if 3 in digits:
+                break
+
+        for pat in inp_segments:
+            if len(pat) == 5:
+                if pat != digits[5] and pat != digits[3]:
+                    digits[2] = pat
+        # assert all(k in digits for k in range(10))
+        # assert all(v != '' for v in digits.values() )
+        
+        reverse_digits = {}
+        for k,v in digits.items():
+            reverse_digits[v] = str(k)
+        import pudb;pu.db
+        assert len(reverse_digits) == 9
+        # for p in inp_segments:
+        #     if p not in reverse_digits:
+        #         reverse_digits[p] = '2'
+            
+        # assert len(digits) == len(reverse_digits)
+        return reverse_digits
+
+    def digest_input(self, part2=False):
+        final_sum = 0
         for l in self._fh_iter:
             inp_segments = l.split("|")[0].split()
             out_segments = l.split("|")[1].split()
-            self.uniq_output_values.extend(list(filter(lambda st: self.filter_uniq(st), out_segments)))
-            for inp_seg in inp_segments:
-                self.analyse_segments(inp_seg)
-            self.disect()
-            import pudb;pu.db
+            self.uniq_output_values.extend(self.collect_uniq(out_segments))
+            if part2:
+                
+                reverse_digits = self.map_digits(inp_segments, out_segments)
+                final_digs = ''
+                for p in out_segments:
+                    for k in reverse_digits:
+                        if set(p) == set(k):
+                            final_digs += reverse_digits[k]
+                    # print(out_segments, final_digs)
+                final_sum += int(final_digs)
+                print(out_segments, final_digs)
+        return final_sum
     
     def digest_input_2(self):
         for l in self._fh_iter:
@@ -61,7 +172,7 @@ class DailyClass:
                 
     def find_obvious(self, inp_sorted, out_sorted) -> dict:
         digits = {}
-        print(f"OUT_SORTED: {out_sorted}")
+        # print(f"OUT_SORTED: {out_sorted}")
         for i in out_sorted:
             
             if len(i) == 2: 
@@ -96,42 +207,13 @@ class DailyClass:
             return self.seg_count_dig_options[len(st)]
         return "??"
 
-    def filter_uniq(self, st):
-        return len(st) in (2,3,4,7)
+    
 
-    def analyse_segments(self, st):
-        if len(st) == 3:
-            self.digit_to_actual_segments[7] = st
-        elif len(st) == 2:
-            self.digit_to_actual_segments[1] = st
-            self.RU = list(st)
-            self.RD = self.RU
-
-        elif len(st) == 7:
-            self.digit_to_actual_segments[8] = st
-        elif len(st) == 4:
-            self.digit_to_actual_segments[4] = st
-
-
-        # if len(st) in (2,3,4,7):
-        #     self.digit_to_actual_segments[st] = self.seg_count_dig_options[len(st)]
-    def disect(self):
-        if all(k in self.digit_to_actual_segments for k in (1,7)):
-            self.U = self.digit_to_actual_segments[7]
-            for _c in self.digit_to_actual_segments[1]:
-                self.U = self.U.replace(_c, '') 
-        if all(k in self.digit_to_actual_segments for k in (1,4)):
-            self.LU = self.digit_to_actual_segments[4]
-            for _c in self.digit_to_actual_segments[1]:
-                self.LU = self.LU.replace(_c, '') 
-            self.LU = list(self.LU)
-            self.M = self.LU
+    
 
     def solve_part1(self):
         self.digest_input()
         return len(self.uniq_output_values)
 
     def solve_part2(self):
-        self.digest_input_2()
-        
-        return "Not Impl"
+        return self.digest_input(part2=True)
