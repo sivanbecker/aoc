e0c8489991ebbf5d48600612ccd8dd3acf4d8222
Sivan Becker <sivanbecker@gmail.com>
2021-12-10 09:04:23 +0200

    2021.day10 part1 done

--- aoc2021/day10/solution.py
+++ aoc2021/day10/solution.py
@@ -1,15 +1,58 @@
+from collections import deque
 class DailyClass:
+    
+    score = {
+        ")": 3,
+        "]": 57,
+        "}": 1197,
+        ">": 25137,
+    }
+
     def __init__(self, fh_iter):
         self._fh_iter = fh_iter
+        self.score_sum = 0
+
+    def find_first_err_in_line(self, l):
+        q = deque()
+        for c in l:
+            if c in ("(", "{", "<", "["):
+                q.append(c)
+            if c == ")" :
+                if q[-1] == "(":
+                    q.pop()
+                else:
+                    self.score_sum += self.score[c]
+                    return
+
+            if c == ">" :
+                if q[-1] == "<":
+                    q.pop()
+                else:
+                    self.score_sum += self.score[c]
+                    return
+
+            if c == "}" :
+                if q[-1] == "{":
+                    q.pop()
+                else:
+                    self.score_sum += self.score[c]
+                    return
+
+            if c == "]" :
+                if q[-1] == "[":
+                    q.pop()       
+                else:
+                    self.score_sum += self.score[c]
+                    return
 
     def digest_input(self):
         for l in self._fh_iter:
             # do something with l
-            pass
+            self.find_first_err_in_line(l.strip())
 
     def solve_part1(self):
         self.digest_input()
-        return "Not Impl"
+        return self.score_sum
 
     def solve_part2(self):
         self.digest_input()
