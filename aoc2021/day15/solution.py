a97eef4640c46e54d532ab4623a2ea9fee1d26d0
Sivan Becker <sivanbecker@gmail.com>
2021-12-15 22:10:11 +0200

    2021.day15 wip

--- aoc2021/day15/solution.py
+++ aoc2021/day15/solution.py
@@ -1,11 +1,21 @@
+import networkx as nx
+import numpy as np
+
 class DailyClass:
     def __init__(self, fh_iter):
         self._fh_iter = fh_iter
+        self.g = None
+
+    def graph_from_input_arr(self, arr):
+        for index, values in np.ndenumerate(arr):
+
+            print(index, values)
 
     def digest_input(self):
+        _arr = np.array(list(next(self._fh_iter).strip()), dtype=int)
         for l in self._fh_iter:
-            # do something with l
-            pass
+            _arr = np.vstack([_arr,list(l.strip())])
+                
 
     def solve_part1(self):
         self.digest_input()
