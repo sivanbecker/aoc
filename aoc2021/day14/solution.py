03ac1967050ace05e96165d850654658e8412344
Sivan Becker <sivanbecker@gmail.com>
2021-12-17 08:37:10 +0200

    2021.day14 part2 wip

--- aoc2021/day14/solution.py
+++ aoc2021/day14/solution.py
@@ -1,23 +1,118 @@
 from collections import defaultdict
 from copy import deepcopy
 import re
-from typing import Generator
+from typing import Counter, Generator
+import datetime
 class DailyClass:
     def __init__(self, fh_iter):
         self._fh_iter = fh_iter
         self.template = None
         self.rules = {}
         self.count = {}
-
+        self.memo = defaultdict(str)
+        
     def digest_input(self):
         self.template = next(self._fh_iter).strip()
-        next(self._fh_iter) # blank line
-
+        next(self._fh_iter) # blank line expected
         for l in self._fh_iter:
-            # do something with l
             pair, toinsert = l.strip().split(" -> ")
             self.rules[pair] = toinsert
+            self.memo[pair] = pair[0]+toinsert+pair[1]
     
+    def calc_3(self, st):
+        return self.memo[st[:2]][:-1]+self.memo[st[1:]]
+
+    def step_with_mem(self, st):
+        # print(f"GOT st={st}")
+        if len(st) == 3:
+            self.memo[st] = self.calc_3(st)
+        if st in self.memo:
+            print(f"using memory {st}")
+            return self.memo[st]
+        print(f"Not in memory {st}")
+
+        self.memo[st] = self.step_with_mem(st[:-2])[:-1] + self.step_with_mem(st[-3:-1])[:-1] + self.step_with_mem(st[-2:])
+        # print(f"inserted to memo {st}")
+        return self.memo[st]
+
+    def memoize(self, st, val):
+        if st not in self.memo:
+            # print(f"Memoizing {st} {val}")
+            self.memo[st] = val
+
+    def run_step(self, st=None) -> str:
+        # print(f"RUN STEP ON {st}")
+        if st is None:
+            st = self.template
+        if len(st) == 3:
+            self.memoize(st, self.calc_3(st))
+
+        if st =='':
+            raise RuntimeError("Empty ST :(")
+        if st in self.memo:
+            # print(f"using memory {st}")
+            return self.memo[st]
+        if st in self.rules:
+            return self.rules[st]
+        # separate into 3 parts (halves + one pair in the middle)
+        stlen = len(st)
+        half_len = int(stlen/2)
+        p1 = st[:half_len]
+        p2 = st[half_len-1:half_len+1]
+        p3 = st[half_len:]
+        # print(f"P1 {p1} ; P2 {p2} ; P3 {p3}")
+        self.memoize(p1, self.run_step(p1))
+        self.memoize(p2, self.run_step(p2))
+        self.memoize(p3, self.run_step(p3))
+        self.memoize(st, self.memo[p1][:-1] + self.memo[p2][:-1] + self.memo[p3])
+        return self.memo[st]
+
+    def run1(self, steps:int = 0) -> str:
+        # st = deepcopy(self.template)
+        while steps:
+            self.template = self.run_step(self.template)
+            steps -=1
+        return self.template
+
+    def run_step2(self, st: int) -> str:
+        print(f"STEP on {st}")
+        print(f"NCN in st {'NCN' in st}")
+        if st in self.memo:
+            print(f"Using value from memory ({len(st)})")
+            return self.memo[st]
+        self.memo[st] = self.run_step2(st[:-1])[:-1] + self.run_step2(st[-2:])
+        print(f"Memorized value for {st} ({len(st)})")
+        return self.memo[st]
+
+    def run2(self, steps:int = 0) -> str:
+        while steps:
+            self.template = self.run_step2(self.template)
+            steps -=1
+        return self.template
+
+    def solve_part1(self):
+        self.digest_input()
+        return self.calc_part1(self.run(steps=10))
+
+    def solve_part2(self):
+        self.digest_input()
+        s = datetime.datetime.now()
+        # end = self.run1(steps=30)
+        print(f"Start with {self.template}")
+        end = self.run2(steps=3)
+        print(end)
+        d = datetime.datetime.now() - s
+        print(f"TOOK {d.seconds} sec")
+        most_common = Counter(self.template).most_common()
+        print(most_common[0][1]-most_common[-1][1])
+
+        # print(end)
+        # for i in range(40):
+        #     n = self.next_poly_generator(n)
+        # return self.calc_part2_1(n)
+        
+##############################################################
+
     def step2(self, lst:Generator = None) -> Generator:
         last = None
         for c in lst:
@@ -108,7 +203,6 @@ class DailyClass:
                 continue
         yield self.recv_st(st)
 
-    
     def runc(self):
         print(f"start with polymer template {self.template}")
         while True:
@@ -146,14 +240,3 @@ class DailyClass:
         # res = max(res, key = res.get)
         return k
 
-    def solve_part1(self):
-        self.digest_input()
-        return self.calc_part1(self.run(steps=10))
-
-    def solve_part2(self):
-        self.digest_input()
-        n = deepcopy(self.template)
-        for i in range(40):
-            n = self.next_poly_generator(n)
-        return self.calc_part2_1(n)
-        
